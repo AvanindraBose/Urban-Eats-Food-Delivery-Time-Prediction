674b8131d24e7d5a72001851cfb0b0d68dc4b237
Avanindra Bose <avanindrabose0218@gmail.com>
2026-05-24 01:40:02 +0530

    Cookie Cutter Project Template is Initialized

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An End to End ML Project for predicting time of delivery for the urban eats application.',
+    author='AvanindraBose',
+    license='MIT',
+)
